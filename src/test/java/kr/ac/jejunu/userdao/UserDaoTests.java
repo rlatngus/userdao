d3c2733abd1e832957e00c3cf5dfc070e29d4677
soohyeon13 <rlatngus6663@gmail.com>
2019-04-05 17:32:07 +0900

    2019-04-05

--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -1,21 +1,24 @@
 package kr.ac.jejunu.userdao;
-
-import javafx.beans.value.ObservableBooleanValue;
+import kr.ac.jejunu.userdao.DaoFactory;
+import kr.ac.jejunu.userdao.User;
+import kr.ac.jejunu.userdao.UserDao;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class UserDaoTests {
 
+public class UserDaoTests {
     @Test
     public void testGet() throws SQLException, ClassNotFoundException {
         Long id = 1l;
         String name = "허윤호";
         String password = "1234";
-        UserDao userDao = new UserDao();
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -29,13 +32,26 @@ public class UserDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        UserDao userDao = new UserDao();
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
         Long id = userDao.add(user);
-        User resultUser =UserDao.get(id);
+        User resultUser = userDao.get(id);
         assertThat(resultUser.getId(), is(id));
         assertThat(resultUser.getName(), is(name));
         assertThat(resultUser.getPassword(), is(password));
-
-
     }
+
+    @Test
+    public void testHallaGet() throws SQLException, ClassNotFoundException {
+        Long id = 1l;
+        String name = "헐크";
+        String password = "1111";
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
+        User user = userDao.get(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
 }
